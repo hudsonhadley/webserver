c80fe0e190e20d7c7cc6f14f4e6509b7565bdda8
Hudson Hadley <hudsonhadley@yahoo.com>
2024-08-09 21:14:03 -0400

    A thread is opened each time a connection is requested.
    There are some issues with this approach. For instance, a devious person could make a script to make 1000 web requests. This could potentially overload our server and cause it to crash. A better approach is to limit the amount of threads or use async.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
+use std::thread;
 use webserver;
 
 fn main() {
     let listener = TcpListener::bind("localhost:80").unwrap();
 
     for stream in listener.incoming() {
-        webserver::handle_connection(stream.unwrap());
+        thread::spawn(|| {
+            webserver::handle_connection(stream.unwrap());
+        });
     }
 }
